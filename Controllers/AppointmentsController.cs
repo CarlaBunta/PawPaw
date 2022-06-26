fe7993235bf7fac056b31441b73fb4a1da0bf0d3
CarlaBunta <56297901+CarlaBunta@users.noreply.github.com>
2022-06-26 20:56:16 +0300

    Update AppointmentsController.cs

--- Controllers/AppointmentsController.cs
+++ Controllers/AppointmentsController.cs
@@ -1,17 +1,64 @@
-﻿using System;
+using HotelBooking.Models;
+using HotelBooking.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace PawPaw.Controllers
+namespace HotelBooking.Controllers
 {
-    public class AppointmentsController : Controller
+    public class AppointmentController : Controller
     {
-        // GET: Appointments
-        public ActionResult Appointments()
+        // GET: Booking
+        private PawPawDBEntities   AppointmentController;
+        public AppointmentController()
         {
-            return View();
+            AppointmentController = new PawPawDBEntities  ();
         }
+        public ActionResult Index()
+        {
+            AppointmentViewModel objAppointmentViewModel = new AppointmentViewModel();
+            objAppointmentViewModel.BookingIn = DateTime.Now;
+
+            return View(objAppointmentViewModel);
+        }
+        [HttpPost]
+        public ActionResult Appointment(AppointmentViewModel objAppointmentViewModel)
+        {
+
+            PatientAppointment patientAppointment = new PatientAppointment()
+            {
+                PatientName=objAppointmentViewModel.PatientName,
+                OwnerName=objAppointmentViewModel.OwnerName,
+                BookingIn=objAppointmentViewModel.BookingIn, 
+                ServiceType=objAppointmentViewModel.ServiceType,
+                TotalAmount=TotalAmount,
+            };
+            AppointmentController.PatientAppointments.Add(PatientAppointment);
+            AppointmentController.SaveChanges();
+
+        
+
+            return Json(data: new { message="Appointments successfully created!", success=true}, JsonRequestBehavior.AllowGet);
+        }
+        public PartialViewResult GetAllAppointmentHistory()
+        {
+            List<PacientAppointmentViewModel> listOfAppointmentViewModels = new List<PacientAppointmentViewModel>();
+            listOfAppointmentViewModels = (from objAppointment in objPawPawDBEntities.PacientAppointments
+                                           join objPacient in objPawPawDBEntities.Pacients on objAppointment.AppointmentId equals objPacient.AppointmentId
+                                           select new PacientAppointmentViewModel
+                                           {
+                                               Bookingin = objAppointment.BookingIn,
+                                               PacientName = objAppointment.PacientName,
+                                               OwnerName = objAppointment.OwnerName,
+                                               ServiceTypeId = objAppointment.ServiceTypeId,
+                                               TotalAmount = objAppointment.TotalAmount,
+                                               ServicePrice = objAppointment.ServicePrice
+                                           }).ToList();
+
+            return PartialView("_AppointmentHistoryPartial", listOfAppointmentViewModels);
+        }
+
     }
 }
