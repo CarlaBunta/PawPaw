04315dbf9627bf9e3d5969867d4625454db1cd10
CarlaBunta <56297901+CarlaBunta@users.noreply.github.com>
2022-06-26 21:04:55 +0300

    Update AppointmentViewModel.cs

--- ViewModel/AppointmentViewModel.cs
+++ ViewModel/AppointmentViewModel.cs
@@ -1,17 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace PawPaw.ViewModel
+namespace Appointment.ViewModel
 {
-    public class AppointmentViewModel : Controller
+    public class AppointmentViewModel
     {
-        // GET: AppointmentViewModel
-        public ActionResult Index()
-        {
-            return View();
-        }
+
+        [Display(Name = "Pacient Name")]
+        [Required(ErrorMessage = "Name is required!")]
+        public string PacientName { get; set; }
+
+        [Display(Name = "Owner Name")]
+        [Required(ErrorMessage = "Name is required!")]
+        public string OwnerName { get; set; }
+
+        [Display(Name = "Customer Phone Number")]
+        [Required(ErrorMessage = "Phone number is required!")]
+        public string BookingIn { get; set; }
+
+        [Display(Name = "Booking In")]
+        [Required(ErrorMessage = "Date is required!")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BookingIn { get; set; }
+
+        [Display(Name = "Service Type")]
+        [Required(ErrorMessage = "Service is required!")]
+        public int ServiceTypeId { get; set; }
+
+        [Display(Name = "Amount")]
+        [Required(ErrorMessage = "Amount is required!")]
+        public int TotalAmount { get; set; }
+
+        public IEnumerable<SelectListItem> ListOfServices { get; set; }
     }
 }
